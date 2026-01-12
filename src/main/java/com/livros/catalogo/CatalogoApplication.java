12f10d24eb67346cdb01bdeaf472674eafb58266
mauricio <mauricioerwork@gmail.com>
2026-01-11 21:48:48 -0300

    fix: remove o spring jpa e driver do mysql

--- src/main/java/com/livros/catalogo/CatalogoApplication.java
+++ src/main/java/com/livros/catalogo/CatalogoApplication.java
@@ -1,5 +1,6 @@
 package com.livros.catalogo;
 
+import com.livros.catalogo.services.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,8 @@ public class CatalogoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("gutendex.com/books?ids=11");
+		System.out.println(json);
 	}
 }
