07ddea9bb9e0e93079a52042c1e8c3d4f5443b3d
mauricio <mauricioerwork@gmail.com>
2026-01-15 07:21:01 -0300

    feat: adiciona parâmetros do autor

--- src/main/java/com/livros/catalogo/model/Autor.java
+++ src/main/java/com/livros/catalogo/model/Autor.java
@@ -1,9 +1,19 @@
 package com.livros.catalogo.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Autor(
-        String name
+        @JsonAlias("name")
+        String name,
+        @JsonAlias("birth_year")
+        Integer AnoNascimento,
+        @JsonAlias("death_year")
+        Integer AnoFalecimento,
+        @JsonAlias("subjects")
+        List<String> livro
 ) {
 }
